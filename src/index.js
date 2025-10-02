bcf5a2c3b18d4ac7d162d4f186e0a5d6f85d6c92
Uday Nagar <uvnagar370823@kkwagh.edu.in>
2025-10-02 22:05:19 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+//The index.js file is intentionally left blank
+
+//This file is the main entry point for your JavaScript application.
+
+//It’s left blank as a placeholder, indicating where you should start writing your code to initialize and run the game.
